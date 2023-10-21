9b147f826dbcbeef6c785cec644637bdf37321d3
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-10-21 17:52:08 -0400

    Implement Layout and CardProspector

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -11,10 +11,13 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
+	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+	public Layout layout;
+	public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this;
@@ -24,14 +27,29 @@ public class Prospector : MonoBehaviour {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle(ref deck.cards);
-		Card c;
+/*		Card c;
 		for (int cNum = 0; cNum < deck.cards.Count; cNum++)
 		{
 			c = deck.cards[cNum];
 			
 			c.transform.localPosition = new Vector3((cNum%13)*3, cNum/13*4,0f);
 
+		}*/
+	
+		layout = GetComponent<Layout> ();
+		layout.ReadLayout(layoutXML.text);
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+	}
+
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+	{
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		foreach(Card tCD in lCD) {
+		tCP = tCD as CardProspector;	
+		lCP.Add(tCP);
 		}
+		return lCP;
 	}
 
 }
