3752c8a2e941b55463a6b2f5551e1fb1a1a1b9c6
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-10-22 00:01:40 -0400

    Configure sorting layers

--- Assets/01-Prospector/__Scripts/Card.cs
+++ Assets/01-Prospector/__Scripts/Card.cs
@@ -4,38 +4,96 @@ using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
+	public string suit;
+	public int rank;
+	public Color color = Color.black;
+	public string colS = "Black";  // or "Red"
+
 	public List<GameObject> decoGOs = new List<GameObject>();
 	public List<GameObject> pipGOs = new List<GameObject>();
-	
+
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+	// List of the SpriteRenderer Components of this GameObject and its children
+	public SpriteRenderer[] spriteRenderers;
 
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
 
-		set {
-			back.SetActive(!value);
-		}
-	}
 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		SetSortOrder(0); //Ensures this card starts properly depth sorted
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
+
 	}
+	// If SpriteRenderers is not already defined, this function defines it
+	public void PopulateSpriteRenderers()
+	{
+		//if spriteRenderers is null or empty
+		if (spriteRenderers == null || spriteRenderers.Length == 0)
+		{
+			// Get SpriteRenderer Components of this GameObject and its children
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	// Sets the sortingLayerName in all SpriteRenderer Components
+	public void SetSortingLayerName(string tSLN)
+	{
+		PopulateSpriteRenderers();
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd)
+	{
+		PopulateSpriteRenderers();
+		// Iterate through all the spriteRenderers as tSR
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			if (tSR.gameObject == this.gameObject)
+			{
+				// If the gameObject is this.gameObject, it's the background
+				tSR.sortingOrder = sOrd;
+				continue;
+
+			}
+			//Each of the children of this gameobject are named
+			//switch based on the name
+			switch (tSR.gameObject.name) 
+			{
+				case "back": // if the naem is "back"
+							 //set it to the highest layer to cover other sprites
+					tSR.sortingOrder = sOrd + 2;
+					break;
+
+				case "face": //If the name is "face" 
+				default: // Or if it's anything else
+						 // Set it to the middle layer to be above the background
+					tSR.sortingOrder = sOrd + 1;
+					break;
+			}
+		}
+	}
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
 } // class Card
 
 [System.Serializable]
--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -12,12 +12,19 @@ public class Prospector : MonoBehaviour {
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
 	public TextAsset layoutXML;
+	public float xOffset = 3;
+	public float yOffset = -2.5f;
+	public Vector3 layoutCenter;
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
 	public Layout layout;
 	public List<CardProspector> drawPile;
+	public Transform layoutAnchor;
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
 
 	void Awake(){
 		S = this;
@@ -39,6 +46,48 @@ public class Prospector : MonoBehaviour {
 		layout = GetComponent<Layout> ();
 		layout.ReadLayout(layoutXML.text);
 		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+		LayoutGame();
+	}
+	CardProspector Draw()
+	{
+		CardProspector cd = drawPile[0];
+		drawPile.RemoveAt(0);
+		return (cd);
+
+	}
+	void LayoutGame()
+	{
+		// Create an empty gameobject to serve as the anchor for the tableau
+		if (layoutAnchor== null)
+		{
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			// Create an empty gameobject called _LayoutAnchor in the heirarchy
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+		CardProspector cp; 
+		// Folow the layout
+		foreach(SlotDef tSD in layout.slotDefs)
+		{
+			// Iterate through all SlotDefs in the layout.slotDefs as tSD
+			cp = Draw();
+			cp.faceUp= tSD.faceUp;
+			cp.transform.parent = layoutAnchor;
+			cp.transform.localPosition = new Vector3(
+				layout.multiplier.x * tSD.x,
+                layout.multiplier.y * tSD.y,
+				-tSD.layerID);
+			// ^ Set the localPosition of the card based on SlotDef
+			cp.layoutID = tSD.id;
+			cp.slotDef= tSD;
+
+			// CardProspectors in the tableau have the state CardState.tableau
+			cp.state = eCardState.tableau;
+
+			cp.SetSortingLayerName(tSD.layerName);
+			tableau.Add(cp); // Add this CardProspector to the List<> tableau
+
+		}
 	}
 
 	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
