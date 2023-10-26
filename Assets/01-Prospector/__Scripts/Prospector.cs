bea2ceb2b6f8853d57945357f1ba9494aa7b6da2
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2023-10-26 18:26:08 -0400

    Remove mysterious line of code from Prospector causing failed builds

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.ShaderKeywordFilter;
 
 public class Prospector : MonoBehaviour {
 
